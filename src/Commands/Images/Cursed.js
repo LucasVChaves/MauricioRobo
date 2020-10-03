85ab1ec426be02287a2232e0623fee16086941a3
Lucas <lucasvchaves@gmail.com>
2020-10-03 19:59:37 -0300

    Cursed Images command added | (NSFW photos too, shhhhhh)

---
+++ src/Commands/Images/Cursed.js
@@ -0,0 +1,33 @@
+const Command = require('../../Structures/Command');
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+
+const subreddits = [
+     "Cursed_Images",
+     "MakeMeSuffer",
+     "NoahGetTheBoat",
+     "thanksihateit",
+     "awfuleverything",
+     "hmmm"
+];
+
+module.exports = class extends Command {
+
+     constructor(...args){
+          super(...args, {
+               aliases: ['cursedpic'],
+               description: 'Manda uma foto aleatória que causa enorme desconforto. Podem ser engraçadas também.',
+               category: "NSFW",
+               nsfw: false         
+          });
+     }
+
+     async run(message) {
+          const data = await fetch(`https://imgur.com/r/${subreddits[Math.floor(Math.random() * subreddits.length)]}/hot.json`)
+               .then(response => response.json())
+               .then(body => body.data);
+
+          const selected = data[Math.floor(Math.random() * data.length)];
+          return message.channel.send(new MessageEmbed().setImage(`https://imgur.com/${selected.hash}${selected.ext.replace(/\?.*/, '')}`));
+     }
+}
---
+++ src/Commands/NSFW/PornGif.js
@@ -0,0 +1,28 @@
+const Command = require('../../Structures/Command');
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+
+const subreddits = [
+
+];
+
+module.exports = class extends Command {
+
+     constructor(...args){
+          super(...args, {
+               aliases: ['gif', 'porng'],
+               description: 'Manda um gif NSFW aleatório.',
+               category: "NSFW",
+               nsfw: true         
+          });
+     }
+
+     async run(message) {
+          const data = await fetch(`https://imgur.com/r/${subreddits[Math.floor(Math.random() * subreddits.length)]}/hot.json`)
+               .then(response => response.json())
+               .then(body => body.data);
+
+          const selected = data[Math.floor(Math.random() * data.length)];
+          return message.channel.send(new MessageEmbed().setImage(`https://imgur.com/${selected.hash}${selected.ext.replace(/\?.*/, '')}`));
+     }
+}
